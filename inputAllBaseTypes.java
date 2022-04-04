4c4effcf397569f76e39b4058f7ef1401190e546
Yasmine Khalel <67972643+Yasmiiiiiine@users.noreply.github.com>
2022-04-04 16:12:01 -0700

    Add files via upload

---
+++ inputAllBaseTypes.java
@@ -0,0 +1,23 @@
+
+package javaapplication25;
+
+/**
+ *
+ * @author Yasmine
+ */
+public class JavaApplication25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //input diff values of each base type 
+        int var1 = 33;
+        double var2 = 3.9;
+        boolean var3 = true;
+        String var4 = "Yasmine";
+        System.out.println("output all the values "+var1 +var2 +var3 + var4);
+        
+    }
+    
+}
