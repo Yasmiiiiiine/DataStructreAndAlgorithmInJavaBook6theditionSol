212df1d42c16d1909145242f210b4d3933333538
Yasmine Khalel <67972643+Yasmiiiiiine@users.noreply.github.com>
2022-04-04 16:33:27 -0700

    Add files via upload

---
+++ isMultiple.java
@@ -0,0 +1,28 @@
+package javaapplication25;
+
+/**
+ *
+ * @author Yasmine
+ */
+public class isMultiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+      
+
+    public boolean isMultiple(int m , int n){
+        int n;
+        int m ;
+        int i = 0;
+        boolean z = true;
+        if (m * i == n) {
+            return z;
+        }
+    }
+
+}
+
+}
